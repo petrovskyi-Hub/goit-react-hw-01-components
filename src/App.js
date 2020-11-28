08a1d46f27a4ad16b98bdb1cc329c281047e5bce
Yaroslav Petrovskyi <yarikpetrovsk@gmail.com>
2020-11-28 18:02:19 +0200

    Виполнение второго задания

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
-import Profile from './components/social-profile/Profile';
-import user from './components/social-profile/user.json';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
+import Statistics from './components/Statistics/Statistics'
+import statisticalData from './components/Statistics/statistical-data.json';
+import FriendList from './components/FriendList/FriendList'
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'
+import transactions from './components/TransactionHistory/transactions.json';
+console.log("🚀 ~ file: App.js ~ line 10 ~ transactions", transactions);
+
 
 function App() {
   return (
@@ -12,6 +20,15 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <div className="Statistics">
+        <Statistics title="Upload stats" stats={statisticalData} />
+        <Statistics stats={statisticalData} />
+      </div>
+
+      <FriendList friends={friends} />
+      
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.css'
+
+export default function Profile({
+  name,
+  tag,
+  location,
+  avatar = "https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg",
+  stats,
+}) {
+  return (
+    <div className={s.profile}>
+    <div className={s.description}>
+      <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className={s.avatar}
+      />
+      <p className={s.name}>{name}</p>
+      <p className={s.tag}>@{tag}</p>
+      <p className={s.location}>{location}</p>
+    </div>
+
+    <ul className={s.stats}>
+      <li className={s.item}>
+        <span className={s.lable}>Followers</span>
+        <span className={s.quantity}>{stats.followers}</span>
+      </li>
+      <li className={s.item}>
+        <span className={s.lable}>Views</span>
+        <span className={s.quantity}>{stats.views}</span>
+      </li>
+      <li className={s.item}>
+        <span className={s.lable}>Likes</span>
+        <span className={s.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.object.isRequired,
+}
